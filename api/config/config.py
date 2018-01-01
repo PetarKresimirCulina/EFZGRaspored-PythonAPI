85b670266a0f52e4f903617b2bf5f5180ba04c5c
Petar-Kresimir Culina <petar@taptoscan.com>
2018-01-01 02:06:09 +0100

    Small changes

--- api/config/config.py
+++ api/config/config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Base(object):
     DEBUG = False
     DEVELOPMENT = False
--- api/config/kreso.py
+++ api/config/kreso.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from api.config import config
 class Local(config.Development):
     pass
